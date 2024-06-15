c0c552b9ed6b16e16d4eff11dd67d631d23e5b7f
TamerlanAbdresh <90834188+TamerlanAbdresh@users.noreply.github.com>
2024-06-15 12:33:05 +0500

    Update task8.java

--- task8.java
+++ task8.java
@@ -11,12 +11,12 @@ public class task8 {
         int n = scanner.nextInt();
 
         double sum = 0.0;
-        int pm = 1;
+        int minpls = 1;
 
         for (int i = 1; i <= n; i ++) {
             double sum1 = 1.0 / (i * 2 - 1);
             sum += pm * sum1;
-            pm = -pm;
+            minpls = -minpls;
         }
 
         System.out.println(sum);
